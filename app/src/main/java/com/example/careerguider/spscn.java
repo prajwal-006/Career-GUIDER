86e75bec0b39da52e6ca1456313d38398c965f3c
Prajwal G <prajwal.gkvk@gmail.com>
2023-02-05 21:41:02 +0530

    FINISHED PROJECT

---
+++ app/src/main/java/com/example/careerguider/spscn.java
@@ -0,0 +1,29 @@
+/*
+package com.example.careerguider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class spscn extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spscn);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(spscn.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
+*/
